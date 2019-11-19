f96508fdfdcfe1c51f362b525b352997e8b79939
makingBug <1751953135@qq.com>
2019-11-19 17:09:40 +0800

    完成了用户部分,优化了部分sql接口的使用细节

--- api/dbops/api.go
+++ api/dbops/api.go
@@ -1,6 +1,7 @@
 package dbops
 
 import (
+	"database/sql"
 	"log"
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -12,8 +13,11 @@ func AddUserCredential(loginName string,pwd string) error  {
 		return err
 	}
 	//将这两个参数按顺序放入问号中
-	stmtIns.Exec(loginName,pwd)
-	stmtIns.Close()
+	_,err=stmtIns.Exec(loginName,pwd)
+	if err != nil{
+		return err
+	}
+	defer stmtIns.Close()
 	return nil
 }
 
@@ -24,8 +28,11 @@ func GetUserCredential(loginName string)(string,error)  {
 		return "",err
 	}
 	var pwd string
-	stmtOut.QueryRow(loginName).Scan(&pwd)
-	stmtOut.Close()
+	err = stmtOut.QueryRow(loginName).Scan(&pwd)
+	if err != nil && err != sql.ErrNoRows{
+		return "", err
+	}
+	defer stmtOut.Close()
 	return pwd, nil
 }
 
@@ -35,7 +42,10 @@ func DeleteUser(loginName string,pwd string) error{
 		log.Printf("DeleteUser error: %s",err)
 		return err
 	}
-	stmtDel.Exec(loginName,pwd)
-	stmtDel.Close()
+	_,err = stmtDel.Exec(loginName,pwd)
+	if err != nil{
+		return err
+	}
+	defer stmtDel.Close()
 	return nil
 }
